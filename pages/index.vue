a3507449658a478191fbe6e7dc83a8aa28c66c97
ymdma <m.yamada1985@gmail.com>
2020-02-20 21:02:13 +0900

    path-name

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,51 @@
           GitHub
         </a>
       </div>
+
+      <hr>
+
+      <div class="test-links">
+        <h3>Users</h3>
+          <ul>
+            <h4>Index</h4>
+            <router-link to="/users">
+              <li>Users/Index</li>
+            </router-link>
+            <router-link :to="{name:'users'}">
+              <li>Users/Index[name]</li>
+            </router-link>
+          </ul>
+          <ul>
+            <h4>Others</h4>
+            <router-link to="/users/others">
+              <li>Users/Others</li>
+            </router-link>
+            <router-link :to="{name:'users-others'}">
+              <li>Users/Others[name]</li>
+            </router-link>
+          </ul>
+
+          <h3>admins</h3>
+          <ul>
+            <h4>Index</h4>
+            <nuxt-link to="/admins">
+              <li>Admins/Index</li>
+            </nuxt-link>
+            <router-link :to="{name:'admins'}">
+              <li>Admins/Index[name]</li>
+            </router-link>
+          </ul>
+
+          <ul>
+            <h4>Others</h4>
+            <router-link to="/admins/others">
+              <li>Admins/Others</li>
+            </router-link>
+            <router-link :to="{name: 'admins-others'}">
+              <li>Admins/Others[name]</li>
+            </router-link>
+          </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -68,5 +113,32 @@ export default {
 
 .links {
   padding-top: 15px;
+  margin: 0 0 20px 0;
 }
+</style>
+
+<style scoped>
+.test-links{
+  margin: 20px 0 0 0;
+  display:flex;
+  flex-direction: column;
+}
+.test-links a,
+.test-links a:visited,
+.test-links a:active{
+color:skyblue;
+}
+h3{
+  margin: 20px;
+  font-size: 24px;
+}
+
+ul{
+  padding: 20px;
+  list-style:none;
+}
+li{
+  padding: 5px;
+}
+
 </style>
---
+++ pages/users/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    This is UserPage.
+
+  </section>
+</template>
+
+<style>
+.container{
+  margin: auto 0;
+  min-height: 100vh;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>>
