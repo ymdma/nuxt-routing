ccb6a206183b9bf77af05e1f811db3033138bf42
ymdma <m.yamada1985@gmail.com>
2020-02-20 22:25:25 +0900

    error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+// ケースごとのエラーを表示
+// 例: 404 ERROR
+
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">404 Error! お探しのページが見つかりません！</h1>
+  </div>
+</template>
+
+<script>
+  export default({
+    props:['error']
+  })
+</script>
